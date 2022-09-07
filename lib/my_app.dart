0dacd2a0cb8886e05a7d9874dfa83c5920f2bd17
t.ngo <ngoquangtuyen2341999@gmail.com>
2022-09-07 23:43:30 +0900

    ログイン画面のレイアウトを作成しました。

---
+++ lib/my_app.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passController = TextEditingController();
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Login Firebase',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text(
+            'Login to your app',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 44.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 44.0,
+          ),
+          TextField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: "Enter your email",
+              prefixIcon: Icon(Icons.email, color: Colors.black),
+            ),
+          ),
+          const SizedBox(
+            height: 26.0,
+          ),
+          TextField(
+            obscureText: true,
+            controller: _passController,
+            decoration: const InputDecoration(
+              hintText: "Enter your password",
+              prefixIcon: Icon(Icons.security, color: Colors.black),
+            ),
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          const Text(
+            'Dont Remember your password?',
+            style: TextStyle(
+                color: Colors.blue,
+                fontSize: 12,
+                fontWeight: FontWeight.bold
+            ),
+          ),
+          const SizedBox(
+            height: 50.0,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: RawMaterialButton(
+              fillColor: const Color(0xFF0069FE),
+              elevation: 0.0,
+              padding: const EdgeInsets.symmetric(vertical: 20.0, ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              onPressed: () {
+              },
+              child: const Text(
+                'Login',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
