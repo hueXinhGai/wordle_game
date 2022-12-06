ff78394e2a2a6bb410b4a8023e1a1689ad4f6e6b
t.ngo <ngoquangtuyen2341999@gmail.com>
2022-12-06 23:01:55 +0900

    set up wordle_game

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
 
-import 'my_app.dart';
-
 void main() {
-  runApp(const MyApp());
-  print('this is test Rebase 01');
-  print('this is test rebase 02');
-  print('this is test rebase 02');
 }
